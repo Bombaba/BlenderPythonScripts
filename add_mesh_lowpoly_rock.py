99aec58d0a1c6cf129c7eed6fa11ef10e60582a3
Hidesato Ikeya <bombombababa@gmail.com>
2013-10-12 10:59:36 +0900

    add_mesh_lowpoly_rock: changed version number to 0.1.5

--- add_mesh_lowpoly_rock.py
+++ add_mesh_lowpoly_rock.py
@@ -22,7 +22,7 @@
 bl_info = {
     "name": "LowPoly Rock",
     "author": "Hidesato Ikeya",
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "blender": (2, 68, 0),
     "location": "VIEW3D > ADD > Mesh",
     "description": "LowPoly Rock",
