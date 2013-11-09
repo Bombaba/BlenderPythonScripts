b180924e78e4ed3e381cc969ff627a4f4544a47a
Hidesato Ikeya <bombombababa@gmail.com>
2013-11-09 20:03:39 +0900

    lowpoly_rock: added random generation

--- add_mesh_lowpoly_rock.py
+++ add_mesh_lowpoly_rock.py
@@ -35,6 +35,7 @@ import bpy
 import bmesh
 from mathutils import Matrix
 from math import radians
+from random import seed, uniform
 
 ROCK_NAME = "LowPolyRock"
 ORIGIN_NAME = ROCK_NAME + "DisplaceOrigin"
@@ -123,41 +124,50 @@ class LowPolyRock(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO', 'PRESET'}
 
     num_rock = bpy.props.IntProperty(
-        name="Number", min=1, max=10, default=1,
+        name="Number", min=1, max=9, default=1,
         description="Number of rocks")
-
-    randomize = bpy.props.BoolProperty(
-        name="Randomize", default=True,
-        description="Randomize")
-
     size = bpy.props.FloatProperty(
         name="Size", min=.0, default=1.0, precision=3, step=0.01)
+    size_ratio = bpy.props.FloatVectorProperty(
+        name="Size Ratio", size=3, min=.0, default=(1., 1., 1.),
+        subtype='TRANSLATION', step=0.1, precision=2, description="Size ratio")
+    displace_midlevel = bpy.props.FloatProperty(
+        name="Midlevel", min=.0, max=1.0, default=.5, precision=3, step=0.1)
     noise_center = bpy.props.FloatVectorProperty(
-        name="Noise Center", step=0.1, subtype='TRANSLATION', size=3,
+        name="Noise Center", size=3, step=0.1, subtype='TRANSLATION',
         description="Displacement noise texture origin")
     simplicity = bpy.props.FloatProperty(
         name="Simplicity", min=.0, max=1.0, default=0.25,
         precision=2, step=0.1, description="Reduce polygons")
     sharpness = bpy.props.FloatProperty(
         name="Sharpness", min=.0, max=2.0, default=.8, precision=3, step=0.1)
-    size_ratio = bpy.props.FloatVectorProperty(
-        name="Size Ratio", size=3, min=.0, default=(1., 1., 1.),
-        subtype='TRANSLATION', step=0.1, precision=2, description="Size ratio")
     edge_split = bpy.props.BoolProperty(
         name="Edge Split", default=True,
         description="Shade smooth and add edge split modifier")
+
+    random_seed = bpy.props.IntProperty(
+        name="Random Seed", min=-1, default=0,
+        description="Randome seed (set -1 to use system clock)")
+    size_min = bpy.props.FloatProperty(
+        name="Size Min", min=-1.0, max=.0, default=-.3, precision=3, step=0.01)
+    size_max = bpy.props.FloatProperty(
+        name="Size Max", min=.0, default=.3, precision=3, step=0.01)
+    size_ratio_min = bpy.props.FloatVectorProperty(
+        name="Ratio Min", size=3, min=-1.0, max=.0, default=(-.2, -.2, -.2),
+        precision=3, step=0.01)
+    size_ratio_max = bpy.props.FloatVectorProperty(
+        name="Ratio Max", size=3, min=.0, default=(.2, .2, .2),
+        precision=3, step=0.01)
 
     keep_modifiers = bpy.props.BoolProperty(
         name="Keep Modifiers", default=False,
         description="Keep modifiers")
     advanced_menu = bpy.props.BoolProperty(
-        name="Advanced", default=False,
+        name="Advanced Menu", default=False,
         description="Display advanced menu")
     voronoi_weights = bpy.props.FloatVectorProperty(
         name="Voronoi Weights", min=-1.0, max=1.0, size=3,
         default=(1.,.3,.0), step=0.1, description="Voronoi Weights")
-    displace_midlevel = bpy.props.FloatProperty(
-        name="Midlevel", min=.0, max=1.0, default=.5, precision=3, step=0.1)
     displace_strength = bpy.props.FloatProperty(
         name="Strength", min=.0, default=1.0, precision=3, step=0.1)
     noise_size = bpy.props.FloatProperty(
@@ -177,40 +187,75 @@ class LowPolyRock(bpy.types.Operator):
 
     def draw(self, context):
         layout = self.layout
-        box = layout.box()
-        box.prop(self, 'num_rock')
-        box.prop(self, 'size')
-        box.prop(self, 'simplicity')
-        box.prop(self, 'sharpness')
-        box.prop(self, 'noise_center')
-        box.prop(self, 'size_ratio')
-        box.prop(self, 'edge_split')
-        layout.prop(self, 'advanced_menu')
+
+        basic = layout.box()
+        basic.label("Basic Settings:")
+        basic.prop(self, 'num_rock')
+        col = basic.column(align=True)
+        col.prop(self, 'size')
+        col.label("Size Ratio:")
+        col.prop(self, 'size_ratio', text="")
+        col.prop(self, 'displace_midlevel')
+
+        col = basic.column(align=True)
+        col.label("Noise Center:")
+        row = col.row()
+        row.prop(self, 'noise_center', text="")
+
+        basic.prop(self, 'simplicity')
+        basic.prop(self, 'sharpness')
+        basic.prop(self, 'edge_split')
+
+        random = layout.box()
+        random.label("Random Settings:")
+        random.prop(self, 'random_seed')
+
+        col = random.column(align=True)
+        col.label('Size Range:')
+        row = col.row(align=True)
+        row.prop(self, 'size_min', text='min')
+        row.prop(self, 'size_max', text='max')
+
+        col = random.column(align=True)
+        col.label('Size Ratio Range:')
+        row = col.row()
+        col = row.column(align=True)
+        col.prop(self, 'size_ratio_min', text="")
+        col = row.column(align=True)
+        col.prop(self, 'size_ratio_max', text="")
+
+        advanced = layout.box()
+        advanced.prop(self, 'advanced_menu', text="Advanced Settings:")
         if self.advanced_menu:
-            box = layout.box()
-            box.prop(self, 'keep_modifiers')
-            box.prop(self, 'displace_midlevel')
-            box.prop(self, 'displace_strength')
-            box.prop(self, 'voronoi_weights')
-            box.prop(self, 'noise_size')
-            box.prop(self, 'noise_brightness')
-            box.prop(self, 'subdiv')
-            box.prop(self, 'collapse_ratio')
+            advanced.prop(self, 'keep_modifiers')
+            advanced.prop(self, 'displace_strength')
+            advanced.prop(self, 'voronoi_weights')
+            advanced.prop(self, 'noise_size')
+            advanced.prop(self, 'noise_brightness')
+            advanced.prop(self, 'subdiv')
+            advanced.prop(self, 'collapse_ratio')
 
     def execute(self, context):
-        location = context.scene.cursor_location.copy()
+        bpy.ops.object.select_all(action='DESELECT')
+
         radius = self.size
+        size_ratio = self.size_ratio.copy()
+        noise_center = self.noise_center.copy()
 
-        for i in range(self.num_rock):
-            bpy.ops.object.select_all(action='DESELECT')
+        random_seed = self.random_seed
+        if random_seed == -1:
+            random_seed = None
+        seed(random_seed)
 
+        location = context.scene.cursor_location.copy()
+        rocks = [None] * self.num_rock
+        for n in range(self.num_rock):
             rock, noise_origin = create_rock(
-                context, self.subdiv, radius, self.size_ratio,
-                self.noise_center, self.noise_size, self.noise_brightness,
+                context, self.subdiv, radius, size_ratio,
+                noise_center, self.noise_size, self.noise_brightness,
                 self.sharpness, self.displace_midlevel, self.displace_strength,
                 self.voronoi_weights, self.simplicity, self.collapse_ratio)
-
-            rock.select = True
+            rocks[n] = rock
 
             if self.keep_modifiers:
                 rock.location = location
@@ -228,13 +273,28 @@ class LowPolyRock(bpy.types.Operator):
                 context.blend_data.textures.remove(tex)
 
             if self.edge_split:
+                rock.select = True
                 bpy.ops.object.shade_smooth()
                 split = rock.modifiers.new('split', 'EDGE_SPLIT')
                 split.use_edge_angle = True
                 split.use_edge_sharp = False
                 split.split_angle = .0
+                rock.select = False
 
-            location.x += radius * 1.5
+            radius = self.size * (1.0 + uniform(self.size_min, self.size_max))
+            for i in range(3):
+                noise_center[i] = self.noise_center[i] + uniform(-1000, 1000)
+                size_ratio[i] = self.size_ratio[i] * \
+                    (1.0 + uniform(self.size_ratio_min[i], self.size_ratio_max[i]))
+            if n % 2 == 0:
+                location.x = context.scene.cursor_location.x \
+                    + self.size * 1.6 * (n // 2 + 1)
+            else:
+                location.x = context.scene.cursor_location.x \
+                    - self.size * 1.6 * (n // 2 + 1)
+
+        for rock in rocks:
+            rock.select = True
 
         return {'FINISHED'}
 
