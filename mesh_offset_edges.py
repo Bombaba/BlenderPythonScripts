c2686321f555129db6e789e9e622f0d9c80e0e8d
Hidesato Ikeya <bombombababa@gmail.com>
2014-05-16 22:34:29 +0900

    offset_edges: added wiki url

--- mesh_offset_edges.py
+++ mesh_offset_edges.py
@@ -27,7 +27,7 @@ bl_info = {
     "location": "VIEW3D > Edge menu(CTRL-E) > Offset Edges",
     "description": "Offset Edges",
     "warning": "",
-    "wiki_url": "",
+    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Modeling/offset_edges",
     "tracker_url": "",
     "category": "Mesh"}
 
